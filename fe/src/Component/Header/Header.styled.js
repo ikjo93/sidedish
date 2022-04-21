1d8a77fd4119d7d312d93ee226196c898f79af5e
BB-choi <choibobae0809@gmail.com>
2022-04-22 08:06:12 +0900

    refactor: Header.styled.js props처리 리팩토링

--- fe/src/Component/Header/Header.styled.js
+++ fe/src/Component/Header/Header.styled.js
@@ -1,24 +1,18 @@
 import styled, { css } from "styled-components";
 
-const paddingByOpened = css`
-  ${({ zzzz }) => {
-    return zzzz
-      ? css`
-          padding: 30px 80px 300px;
-        `
-      : css`
-          padding: 30px 80px;
-        `;
-  }}
-`;
-
 const StyledHeader = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
   /* TODO: 공통패딩 분리 */
   border-bottom: 1px solid #1b1b1b;
-  ${paddingByOpened}
+  padding: 30px 80px;
+
+  ${({ isOpen }) =>
+    isOpen &&
+    css`
+      padding: 30px 80px 300px;
+    `}
 `;
 
 export default StyledHeader;
