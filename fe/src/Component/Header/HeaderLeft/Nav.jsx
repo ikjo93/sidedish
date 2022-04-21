12554e691bf8854b1e081cd6c21037edeac3a162
BB-choi <choibobae0809@gmail.com>
2022-04-21 23:16:49 +0900

    feat: 서브메뉴 생성 및 Proptypes 설정

--- fe/src/Component/Header/HeaderLeft/Nav.jsx
+++ fe/src/Component/Header/HeaderLeft/Nav.jsx
@@ -1,8 +1,28 @@
+import PropTypes from "prop-types";
 import { StyledNav, StyledUl } from "./Nav.styled";
 import { MenuDatas } from "../../../MockData/MockData";
 
+const SubMenu = ({ subMenuDatas }) => {
+  const subMenuList = subMenuDatas.map((subMenuData) => (
+    <li key={subMenuData.id}>{subMenuData.name}</li>
+  ));
+  return <ul>{subMenuList}</ul>;
+};
+
+SubMenu.propTypes = {
+  subMenuDatas: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
 const menus = MenuDatas.map((MenuData) => (
-  <li key={MenuData.id}>{MenuData.name}</li>
+  <li key={MenuData.id}>
+    {MenuData.name}
+    <SubMenu subMenuDatas={MenuData.subMenu} />
+  </li>
 ));
 
 const Nav = () => {
