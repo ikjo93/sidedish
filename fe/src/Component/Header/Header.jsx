4c09a2b999b22bfa567dec1f99421b3b525e54da
BB-choi <choibobae0809@gmail.com>
2022-04-22 11:52:08 +0900

    feat: 서브메뉴 아이템 수에따라 header크기 조절

--- fe/src/Component/Header/Header.jsx
+++ fe/src/Component/Header/Header.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
+import { MenuDatas } from "../../MockData/MockData";
 import StyledHeader from "./Header.styled";
 import HeaderRight from "./HeaderRight/HeaderRight";
 import HeaderLeft from "./HeaderLeft/HeaderLeft";
+
+const maxSubMenuDataSize = MenuDatas.reduce((maxLength, curMenuData) => {
+  const curLength = curMenuData.subMenu.length;
+  return curLength > maxLength ? curLength : maxLength;
+}, 0);
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,7 +21,7 @@ const Header = () => {
   };
 
   return (
-    <StyledHeader isOpen={isOpen}>
+    <StyledHeader isOpen={isOpen} maxSubMenuDataSize={maxSubMenuDataSize}>
       <HeaderLeft state={{ handleMouseEvent, checkIsOpen }} />
       <HeaderRight />
     </StyledHeader>
--- fe/src/Component/Header/Header.styled.js
+++ fe/src/Component/Header/Header.styled.js
@@ -1,17 +1,19 @@
 import styled, { css } from "styled-components";
 
+const defaultPaddingBottom = 30;
+
 const StyledHeader = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  /* TODO: 공통패딩 분리 */
   border-bottom: 1px solid #1b1b1b;
+  /* TODO: 공통패딩 분리 */
   padding: 30px 80px;
 
-  ${({ isOpen }) =>
+  ${({ isOpen, maxSubMenuDataSize }) =>
     isOpen &&
     css`
-      padding: 30px 80px 300px;
+      padding: 30px 80px ${24 * maxSubMenuDataSize + defaultPaddingBottom}px;
     `}
 `;
 
