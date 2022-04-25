43c0d2bc526bce7cf9c2377d8e17e557b53f1e1a
BB-choi <choibobae0809@gmail.com>
2022-04-25 21:32:55 +0900

    fix: 불필요한 padding 속성 제거

--- fe/src/Component/Header/Header.styled.js
+++ fe/src/Component/Header/Header.styled.js
@@ -7,13 +7,10 @@ const StyledHeader = styled.header`
   justify-content: space-between;
   align-items: center;
   border-bottom: 1px solid #1b1b1b;
-  /* TODO: 공통패딩 분리 */
-  padding: 30px 80px;
-
   ${({ isOpen, maxSubMenuDataSize }) =>
-    isOpen &&
     css`
-      padding: 30px 80px ${24 * maxSubMenuDataSize + defaultPaddingBottom}px;
+      padding: 30px 80px
+        ${isOpen && `${24 * maxSubMenuDataSize + defaultPaddingBottom}px`};
     `}
 `;
 
