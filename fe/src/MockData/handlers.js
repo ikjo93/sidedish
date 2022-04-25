5c4e5b170e7e4764f56a93176d7852779c675d40
JinJeon <nanyong01@gmail.com>
2022-04-25 12:25:30 +0900

    fix: 기획전 카테고리 api 수정

--- fe/src/MockData/handlers.js
+++ fe/src/MockData/handlers.js
@@ -5,7 +5,7 @@ const handlers = [
   rest.get("/api/event-categories", (req, res, ctx) => {
     return res(ctx.status(200), ctx.delay(100), ctx.json(event));
   }),
-  rest.get(/[0-9]$/, (req, res, ctx) => {
+  rest.get(`/api/event-categories/${/[0-9]$/}/sidedishes`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.delay(100), ctx.json(categoryMenus));
   }),
   rest.get(/(img)*/, (req, res, ctx) => {
