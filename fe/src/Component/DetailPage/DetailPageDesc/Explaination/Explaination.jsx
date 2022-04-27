2c1ab15a7160f227dacc854f87c1a8dab51fb5fb
JinJeon <nanyong01@gmail.com>
2022-04-28 01:47:04 +0900

    feat: 서브 이미지 클릭 시 이미지 변경 및 선택된 이미지 표시

--- fe/src/Component/DetailPage/DetailPageDesc/Explaination/Explaination.jsx
+++ fe/src/Component/DetailPage/DetailPageDesc/Explaination/Explaination.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
-import DetailInfoContext from "Component/DetailPage/DetailInfoContext";
-import { useContext } from "react";
+import Images from "./Images/Images";
 
 const ExplainationDiv = styled.div`
   display: flex;
@@ -9,43 +8,11 @@ const ExplainationDiv = styled.div`
   justify-content: space-between;
 `;
 
-const PictureDiv = styled.div`
-  border: red solid 1px;
-  width: 392px;
-`;
-
 const OrderDiv = styled.div`
   border: 1px solid blue;
   width: 440px;
 `;
 
-const MainImage = styled.img`
-  width: 392px;
-`;
-
-const SubImages = styled.div`
-  margin-top: 8px;
-  img {
-    width: 72px;
-    border-right: 8px;
-  }
-`;
-
-const Images = () => {
-  const { imageFiles, name } = useContext(DetailInfoContext);
-  const subImageFiles = imageFiles.map((imageFile, idx) => {
-    return { src: imageFile, idx };
-  });
-  const subImages = subImageFiles.map(({ src, idx }) => {
-    return <img src={src} key={idx} alt={name} />;
-  });
-  return (
-    <PictureDiv>
-      <MainImage src={`${imageFiles[0]}`} alt="test" />
-      <SubImages>{subImages}</SubImages>
-    </PictureDiv>
-  );
-};
 const Order = () => {
   return <OrderDiv />;
 };
---
+++ fe/src/Component/DetailPage/DetailPageDesc/Explaination/Images/Images.jsx
@@ -0,0 +1,37 @@
+import DetailInfoContext from "Component/DetailPage/DetailInfoContext";
+import { useContext, useState } from "react";
+import { ImagesDiv, MainImage, SubImagesArea, SubImage } from "./Images.styled";
+
+const Images = () => {
+  const [imageNumber, setImageNumber] = useState(0);
+  const { imageFiles, name } = useContext(DetailInfoContext);
+  const subImageFiles = imageFiles.map((imageFile, idx) => {
+    return { src: imageFile, idx };
+  });
+
+  const handleSubImageClick = ({ target: { id } }) => {
+    setImageNumber(Number(id));
+  };
+
+  const subImages = subImageFiles.map(({ src, idx }) => {
+    return (
+      <SubImage
+        onClick={handleSubImageClick}
+        src={src}
+        key={idx}
+        id={idx}
+        imageNumber={imageNumber}
+        alt={name}
+      />
+    );
+  });
+
+  return (
+    <ImagesDiv>
+      <MainImage src={`${imageFiles[imageNumber]}`} alt="test" />
+      <SubImagesArea id={imageNumber}>{subImages}</SubImagesArea>
+    </ImagesDiv>
+  );
+};
+
+export default Images;
