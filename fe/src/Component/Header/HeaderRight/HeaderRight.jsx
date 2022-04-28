c0080c1e8a5683834a7459afe81c3c6a4a5a7f25
BB-choi <choibobae0809@gmail.com>
2022-04-29 04:15:27 +0900

    fix: 불필요한 테스트 코드 삭제

--- fe/src/Component/Header/HeaderRight/HeaderRight.jsx
+++ fe/src/Component/Header/HeaderRight/HeaderRight.jsx
@@ -1,25 +1,15 @@
 import ButtonWrap from "Component/Header/HeaderRight/HeaderRight.styled";
 import { icons } from "MockData/MockData";
-// import { useEffect } from "react";
-// import loginApi from "Service/loginApi";
 
 const Buttons = () => {
-  // useEffect(() => {
-  //   const fetchLoginData = async () => {
-  //     const response = await loginApi.getLoginInfo();
-
-  //   };
-  //   console.log(fetchLoginData);
-  // }, []);
-
   return icons.map((icon) => (
-    <a
+    <button
       href="https://github.com/login/oauth/authorize?client_id=52d17ef5577e55a18ca3&scope=user:email"
       type="button"
       key={icon.id}
     >
       {icon.svg}
-    </a>
+    </button>
   ));
 };
 
